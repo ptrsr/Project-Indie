7fff588d002d1f6ad8f9875c7f2a234ab615d0d2
Ruud Peters <ruud_peters96@hotmail.com>
2017-11-23 15:09:16 +0100

    temp

---
+++ Assets/Scripts/Menu/CustomButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CustomButton : Button {
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        print(true);
+        base.OnSelect(eventData);
+    }
+
+    public override Selectable FindSelectableOnRight()
+    {
+        print(false);
+        return base.FindSelectableOnRight();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        foreach (string joyName in Input.GetJoystickNames())
+        {
+            print(joyName);
+        } 
+
+	}
+}
