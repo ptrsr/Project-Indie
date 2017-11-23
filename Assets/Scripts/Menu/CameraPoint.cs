d82aa6c7fe2304c3ba6ebbf83265721c96a8e263
Ruud Peters <ruud_peters96@hotmail.com>
2017-11-23 13:34:34 +0100

    temp

--- Assets/Scripts/Menu/CameraPoint.cs
+++ Assets/Scripts/Menu/CameraPoint.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CameraPoint : MonoBehaviour
 {
     [SerializeField]
@@ -18,5 +19,22 @@ public class CameraPoint : MonoBehaviour
     public float ArriveSpeed
     {
         get { return _arriveSpeed; }
+    }
+
+    private void Update()
+    {
+        if (Application.isPlaying)
+            return;
+
+        CameraMovement movement = ServiceLocator.Locate<CameraMovement>();
+
+        if (movement == null || !movement.transform.hasChanged)
+            return;
+
+        if (movement.CurrentPoint == this)
+        {
+            movement.transform.position = transform.position;
+            movement.transform.rotation = transform.rotation;
+        }
     }
 }
