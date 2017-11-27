71d6808826a2b2a8caaae28f07f273ddc367fd2e
Ruud Peters <ruud_peters96@hotmail.com>
2017-11-27 12:36:53 +0100

    Input handler fixed

--- Assets/Scripts/Menu/CustomInputHandler.cs
+++ Assets/Scripts/Menu/CustomInputHandler.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Players;
 
 public class CustomInputHandler : StandaloneInputModule
 {
+    protected override void Awake()
+    {
+        base.Awake();
+
+        ServiceLocator.Provide(this);
+    }
+
     public PointerEventData GetLastPointerEventDataPublic()
     {
         return GetLastPointerEventData(-1);
+    }
+
+    public void SetPlayer(Player player)
+    {
+        string playerName = player.ToString();
+
+        horizontalAxis = playerName + "HorMove";
+        verticalAxis = playerName + "VerMove";
     }
 }
