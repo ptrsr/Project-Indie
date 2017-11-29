da4ef58f97ba0d0c50a1cb1543f2f5847848cd94
Espen Lindahl <venomcoffee@gmail.com>
2017-11-29 11:26:57 +0100

    UI and music

---
+++ Assets/Scripts/ButtonAnimController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonAnimController : MonoBehaviour {
+
+    private void Update()
+    {
+        //For Testing ---
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ButtonSelected();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ButtonReturn();
+        }
+        // ---
+    }
+    public void ButtonSelected()
+    {
+        gameObject.GetComponent<Animation>().Play("ButtonSelected");
+    }
+    public void ButtonReturn()
+    {
+        gameObject.GetComponent<Animation>().Play("ButtonReturn");
+    }
+
+
+}
