8aff40fe2f21d7761729237686c50b3803d6d0ee
Ruud Peters <ruud_peters96@hotmail.com>
2017-11-29 01:28:26 +0100

    raw version of scrollrect

--- Assets/Scripts/Menu/CustomScrolRect.cs
+++ Assets/Scripts/Menu/CustomScrolRect.cs
@@ -29,13 +29,11 @@ public class CustomScrolRect : ScrollRect
         float bottom = verticalNormalizedPosition * (_contentHeight - _viewHeight);
         float top = bottom + _viewHeight;
 
-        print(buttonHeight - bottom - (transform.rect.height / 2));
-        if (buttonHeight - bottom - (transform.rect.height / 2) < _minDist)
-            verticalNormalizedPosition = Mathf.Clamp01((buttonHeight - (transform.rect.height / 2) - _minDist + (_viewHeight)) / _contentHeight);
+        float move = 0;
 
-        if (buttonHeight - top - (transform.rect.y / 2) > -_minDist)
-            verticalNormalizedPosition = Mathf.Clamp01((buttonHeight + (transform.rect.height / 2) + _minDist - (_viewHeight / 2)) / _contentHeight);
+        move += Mathf.Min(buttonHeight - bottom - transform.rect.height / 2 - _minDist, 0);
+        move += Mathf.Max(buttonHeight - top + transform.rect.height / 2 + _minDist, 0);
 
-        //    print(true);
+        verticalNormalizedPosition = Mathf.Clamp01(verticalNormalizedPosition + move / _viewHeight);
     }
 }
